62773ae1d54364bdb29c58c9d719fda0b3ffa191
Ivan Sokolov <hutenbergg@gmail.com>
2015-05-29 17:41:21 +0400

    Update Strings

--- src/main/java/com/igumnov/common/Strings.java
+++ src/main/java/com/igumnov/common/Strings.java
@@ -1,18 +1,14 @@
 package com.igumnov.common;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Strings {
 
     public static Stream<Character> stream(String s) {
-        int len = s.length();
-        Character[] chars = new Character[len];
-        for (int i = 0; i < len ; i++) {
-            chars[i] = new Character(s.charAt(i));
+        Stream.Builder<Character> builder = Stream.builder();
+        for(int i = 0, length = s.length(); i < length; i++){
+            builder.add(s.charAt(i));
         }
-        ArrayList<Character> line = new ArrayList<Character>(Arrays.asList(chars));
-        return line.stream();
+        return builder.build();
     }
 }
